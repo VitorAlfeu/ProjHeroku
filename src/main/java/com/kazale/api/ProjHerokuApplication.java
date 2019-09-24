8eb86a41bc5edaf8408e383f30dc2280276bbc72
velioterio <velioterio@dxc.com>
2019-09-24 10:02:03 -0300

    Subindo projeto de referência com Heroku

---
+++ src/main/java/com/kazale/api/ProjHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.kazale.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjHerokuApplication.class, args);
+	}
+}
